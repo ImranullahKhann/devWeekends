ef816b9857ce7f31e8a8e781450874bdb2e8021b
Imranullah Khan <imranullahkhann2004@gmail.com>
2026-05-03 00:11:15 +0500

    setup auth on server

---
+++ mern-auth/server/config/mongodb.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    mongoose.connection.on("connected", () => console.log("Connected to db"));
+    
+    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+}
+
+export default connectDB;
